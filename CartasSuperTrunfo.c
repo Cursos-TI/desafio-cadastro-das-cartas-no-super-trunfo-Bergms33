b0e052e1bad8dff40145457f34de2436c0ea07d0
Bergms33 <Bergface333@Gmail.com>
2025-04-02 18:43:13 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,10 +4,23 @@ int main() {
     printf("Super Trunfo!\n");
     printf("comit\n");
 
+    char estado = 'A';
+    char codigodacarta[20] = "A01";
+    char nomedacidade[20] = "Amazonas";
+    float populacao = 20.636890; 
+    float area = 11.401902;
+    float pib = 103.281;
+    int numerodepontosturisticos = 10;
 
-
-
-
+    printf("Estado: %c\n" ,estado);
+    printf("Código da Cidade: %s\n" ,codigodacarta);
+    printf("Nome da Cidade: %s\n" ,nomedacidade);
+    printf("Populaçao: %.6f\n" ,populacao);
+    printf("Área: %.3f\n", area);
+    printf("PIB: %.3f\n", pib);
+    printf("Número de Pontos Turisticos: %d\n", numerodepontosturisticos);
 
     return 0;
-}
+  }
+
+
